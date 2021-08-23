eba3576cf770494f778ce699e6f9173ad9cf20ed
Sudeep Ratnaparkhe <sudeeppr1998@gmail.com>
2021-08-23 23:32:56 +0530

    new site update

---
+++ assets/js/main.js
@@ -0,0 +1,99 @@
+function button1() {
+  document.getElementById('myImage').src = "assets/images/q.png";
+
+  var text1 = document.getElementById("text1");
+
+  if (text1.style.display === "none") {
+    text1.style.display = "block";
+    text2.style.display = "none";
+    text3.style.display = "none";
+    text4.style.display = "none";
+    text5.style.display = "none";
+  }
+  /*else
+  {
+    text1.style.display = "none";
+  }
+
+//document.getElementById('message').innerHTML="IOt..";
+//document.write("pratiksha")
+*/
+
+}
+
+function button2() {
+  document.getElementById('myImage').src = "assets/images/w.jpg";
+
+  var text2 = document.getElementById("text2");
+
+  if (text2.style.display === "none") {
+    text2.style.display = "block";
+    text1.style.display = "none";
+    text3.style.display = "none";
+    text4.style.display = "none";
+    text5.style.display = "none";
+  }
+  /* else
+   {
+     text2.style.display = "none";
+   }
+
+ //document.getElementById('message').innerHTML="service 1";
+ */
+}
+
+function button3() {
+  document.getElementById('myImage').src = "assets/images/e.png";
+  var text3 = document.getElementById("text3");
+
+  if (text3.style.display === "none") {
+    text3.style.display = "block";
+    text1.style.display = "none";
+    text2.style.display = "none";
+    text4.style.display = "none";
+    text5.style.display = "none";
+  }
+  /* else
+   {
+     text3.style.display = "none";
+   }
+ //document.getElementById('message').innerHTML="service 4";
+ */
+}
+
+function button4() {
+  document.getElementById('myImage').src = "assets/images/r.jpg";
+  var text4 = document.getElementById("text4");
+
+  if (text4.style.display === "none") {
+    text4.style.display = "block";
+    text1.style.display = "none";
+    text2.style.display = "none";
+    text3.style.display = "none";
+    text5.style.display = "none";
+  }
+  /* else
+   {
+     text4.style.display = "none";
+   }
+ //document.getElementById('message').innerHTML="service 3";
+ */
+}
+function button5() {
+  document.getElementById('myImage').src = "assets/images/r.jpg";
+  var text5 = document.getElementById("text5");
+
+  if (text5.style.display === "none") {
+    text5.style.display = "block";
+    text1.style.display = "none";
+    text2.style.display = "none";
+    text3.style.display = "none";
+    text4.style.display = "none";
+  }
+  /* else
+   {
+     text4.style.display = "none";
+   }
+ //document.getElementById('message').innerHTML="service 3";
+ */
+}
